f1dfe087f037d930e0cb0f8e3bc4722358dd5b19
Richard Scott Salchunas <sccrlx4ss2@yahoo.com>
2018-03-27 05:09:18 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -35,11 +35,11 @@ class TicTacToe
     @board[@index] = value
   end
 
-  def position_taken?(index)
+  def position_taken?
     @board[@index] == "X" || @board[@index] == "O"
   end
 
-  def valid_move?(index)
+  def valid_move?
     index.between?(0,8) && !position_taken?(index)
   end
 
